fb4b6c612d016081a793bed28b7fb28cb18d4e39
kyeimuda <kyeibernard01@gmail.com>
2022-02-16 18:06:32 -0800

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ if (new_node == NULL)
 return (NULL);
 new_node->n = value;
 new_node->parent = parent;
-new_node->right = NULL;
+new_node->left = NULL;
 new_node->right = NULL;
 if (parent == NULL)
 return (NULL);
